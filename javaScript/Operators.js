3c73c1002a1703240feb8b733333f9affedc32de
Ahmed388 <43072044+Ahmed388@users.noreply.github.com>
2019-09-19 13:44:23 -0600

    update

---
+++ javaScript/Operators.js
@@ -0,0 +1,25 @@
+console.log("You made it to the console!")
+
+let number = [4, 7, 13, 17, 18, 20, 99, 100, '1']
+
+function simpleOperators (numberA, numberB) {
+  let adding = numberA + numberB;
+  let subtraction = numberA - numberB;
+  let multiplying = numberA * numberB;
+  let division = numberA / numberB;
+  let modulus = numberA % numberB;
+  console.log(numberA, numberB, 'adding', adding);
+  console.log(numberA, numberB, 'subtraction', subtraction);
+  console.log(numberA, numberB, 'multiplying', multiplying);
+  console.log(numberA, numberB, 'division', division);
+  console.log(numberA, numberB, 'modulus', modulus);
+}
+
+// simpleOperators
+
+skip count by ____ for ____ times starting at ____
+function skipCounting (interveral, times, startingNumber)
+
+simpleOperators(number[0], number[1])
+simpleOperators(number[2], number[3])
+simpleOperators(number[4], number[5])
