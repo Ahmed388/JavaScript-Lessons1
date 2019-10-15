3150769814f0c5f2436af1ae4a4c3d8d64410e21
Ahmed388 <43072044+Ahmed388@users.noreply.github.com>
2019-10-15 13:52:50 -0600

    update

--- javaScript/modulus.js
+++ javaScript/modulus.js
@@ -1,1 +1,13 @@
-console.log("Did you change my file name?")
+//console.log("Did you change my file name?")
+
+for (var x=0; x<=20; x++) {  //The numbers we want to measure
+        if (x === 0) {  //x starts from zero
+                console.log(x +  " is even"); // Numebr is even, eg( 2/4/6/8)
+        }
+        else if (x % 2 === 0) {
+                console.log(x + " is even"); //number is even, eg (2/4/8)
+        }
+        else {
+                console.log(x + " is odd"); //Number is odd, very odd? 1/3/5/7, me when I get 99% on a test
+        }
+}
